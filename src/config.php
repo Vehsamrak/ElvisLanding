7b6d5f63601171e556b8eebef45a94a8eb3ffb39
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2016-04-14 23:30:47 +0300

    Configuration file added

---
+++ src/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'database_type'     => 'mysql',
+    'database_host'     => 'localhost',
+    'database_user'     => 'root',
+    'database_password' => '',
+    'database_name'     => 'elvisland',
+    'root_directory'    => __DIR__,
+];
