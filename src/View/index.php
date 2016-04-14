b6b25fe9c9540bd1d3f5051a4feb61762f411f92
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2016-04-14 23:37:29 +0300

    Styles css extracted to separated file

--- src/View/index.php
+++ src/View/index.php
@@ -1,41 +1,8 @@
-<style>
-
-    .center {
-        display: block;
-        text-align: center;
-    }
-
-    h1 {
-        display: block;
-        padding: 30px;
-        padding-top: 60px;
-        font-size: 60px;
-    }
-
-    .feature {
-        display: inline-block;
-        background-color: chartreuse;
-        padding: 60px 100px;
-        margin: 10px;
-    }
-
-    .break-30 {
-        padding: 30px;
-    }
-
-    .newsbox-title {
-        font-size: 28px;
-        display: block;
-        padding: 20px;
-        background-color: #E4FF00;
-    }
-
-    .newsbox .news {
-        background-color: #F1FAA7;
-        padding: 20px;
-        margin: 10px;
-    }
-</style>
+<html>
+<head>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+</html>
 
 <div class="center">
     <h1>Elvis</h1>
