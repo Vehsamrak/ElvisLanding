ab758e7e0e5565d521f3437e5a8093d47add77cd
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2016-04-14 23:31:31 +0300

    Index.php with autoloading

---
+++ web/index.php
@@ -0,0 +1,22 @@
+<?php
+
+use Vehsamrak\Vehsa\Router;
+
+define('ROOT_DIRECTORY', join(DIRECTORY_SEPARATOR, [__DIR__, '..', 'src']));
+require join(DIRECTORY_SEPARATOR, [ROOT_DIRECTORY, '..', 'vendor', 'autoload.php']);
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+spl_autoload_register(function ($className) {
+    $className = str_replace('\\', '/', $className);
+    $namespace = str_replace('\\', '/', __NAMESPACE__) ?: '/';
+    $class = ROOT_DIRECTORY . $namespace . $className . '.php';
+
+    if (file_exists($class)) {
+        include_once($class);
+    }
+});
+
+$router = new Router();
+$controller = $router->run();
